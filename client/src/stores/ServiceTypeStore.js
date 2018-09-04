794bd064faeb00e5987c1cccd3c95ed53d0910c4
Ivica Sapina <ivica.sapina@domnet.ba>
2018-09-04 09:35:20 +0200

    Add ServiceTypeStore

---
+++ client/src/stores/ServiceTypeStore.js
@@ -0,0 +1,132 @@
+import _ from 'lodash';
+import { action, observable } from 'mobx';
+import * as service from '../services/serviceTypeService';
+
+class ServiceTypeStore {
+  @observable serviceType = {
+    name: '',
+    oilChangeService: false,
+    intervalService: false,
+    inspectionService: false,
+    brakeFluidChanged: false,
+    longLifeOilUsed: false,
+    atf: false,
+    dsgOilAndFilter: false,
+    naturalGasTankAndPipelines: false,
+    haldexCouplingOil: false,
+    fuelFilter: false,
+    airFilter: false,
+    tirePressureSensors: false,
+    tireFillerBottle: false,
+    dustAndPollenFilter: false,
+    toothedBelt: false,
+    sparkPlugs: false
+  }
+  @observable loading = false;
+  @observable serviceTypes = [];
+  @observable errors = {};
+
+  @action
+  saveServiceType = async (history) => {
+    try {
+      await service.save([this.serviceType]);
+      this.refreshStateToInitialValue();
+
+      history.push('/car-service');
+    } catch (error) {
+      this.errors = error.response.data;
+    }
+  }
+
+  @action
+  findOne = async (id, embeds) => {
+    try {
+      const res = await service.findOne(id, embeds);
+      this.serviceType = _.pick(res.data.data[0], ['name', 'oilChangeService', 'intervalService', 'inspectionService', 'brakeFluidChanged', 'longLifeOilUsed',
+        'atf', 'dsgOilAndFilter', 'naturalGasTankAndPipelines', 'haldexCouplingOil', 'fuelFilter', 'airFilter', 'tirePressureSensors', 'tireFillerBottle', 'dustAndPollenFilter', 'toothedBelt', 'sparkPlugs']);
+
+    } catch (error) {
+      this.errors = error.response.data;
+    }
+  }
+
+  @action
+  find = async (params) => {
+    try {
+      this.loading = true;
+      const res = await service.find(params);
+      if (!_.isEmpty(res.data.data)) {
+        this.serviceTypes = res.data.data;
+      }
+      else {
+        this.serviceTypes = [];
+      }
+      this.loading = false;
+    } catch (error) {
+      this.errors = error.response.data;
+      this.loading = false;
+    }
+  }
+
+  @action
+  updateServiceType = async (id) => {
+    try {
+      await service.update(id, this.serviceType);
+      this.refreshStateToInitialValue();
+    } catch (error) {
+      this.errors = error.response.data;
+    }
+  }
+
+  @action
+  removeFromList = (id) => {
+    _.remove(this.serviceTypes, e => e.id === id);
+  }
+
+  @action
+  onRemoveServiceType = async (id) => {
+    try {
+      await service.remove(id);
+      _.remove(this.serviceTypes, e => e.id === id);
+    } catch (error) {
+      this.errors = error.response.data;
+    }
+  }
+
+  @action
+  onChange = (e) => {
+    this.serviceType[e.target.name] = e.target.value;
+  }
+
+  @action
+  onSelect = (id, history) => {
+    history.push(`/car-service/service-types/${id}`);
+  }
+
+  @action
+  refreshStateToInitialValue = () => {
+    this.serviceType.name = '';
+    this.serviceType.oilChangeService = false;
+    this.serviceType.ntervalService = false;
+    this.serviceType.inspectionService = false;
+    this.serviceType.brakeFluidChanged = false;
+    this.serviceType.longLifeOilUsed = false;
+    this.serviceType.atf = false;
+    this.serviceType.dsgOilAndFilter = false;
+    this.serviceType.naturalGasTankAndPipelines = false;
+    this.serviceType.haldexCouplingOil = false;
+    this.serviceType.fuelFilter = false;
+    this.serviceType.airFilter = false;
+    this.serviceType.tirePressureSensors = false;
+    this.serviceType.tireFillerBottle = false;
+    this.serviceType.dustAndPollenFilter = false;
+    this.serviceType.toothedBelt = false;
+    this.serviceType.sparkPlugs = false
+    this.serviceTypes = [];
+    this.errors = {};
+  }
+}
+
+const serviceTypeStore = new ServiceTypeStore();
+
+export default serviceTypeStore;
